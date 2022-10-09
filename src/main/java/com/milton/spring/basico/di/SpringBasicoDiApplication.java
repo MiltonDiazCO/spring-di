670b8cfb0e6b4955c44c7815ca2c06d068f98b52
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-10-09 14:25:58 -0500

    Initial commit

---
+++ src/main/java/com/milton/spring/basico/di/SpringBasicoDiApplication.java
@@ -0,0 +1,13 @@
+package com.milton.spring.basico.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicoDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicoDiApplication.class, args);
+	}
+
+}
